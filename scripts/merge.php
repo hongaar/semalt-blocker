8db23c6d1a72d9ce6f587ea0082e9badeaca62e1
hongaar <hongaar@gmail.com>
2015-06-16 14:21:59 +0200

    added sources from https://github.com/antispam/false-referrals

---
+++ scripts/merge.php
@@ -0,0 +1,28 @@
+<?php
+require_once('./../vendor/autoload.php');
+
+$semaltBlockerSources = \Nabble\Semalt::getBlocklist();
+
+echo "Old list: " . count($semaltBlockerSources) . " sources\n";
+echo "Paste domains (one on each line), then press ENTER twice:\n\n";
+
+$handle = fopen ("php://stdin","r");
+$newDomains = [];
+while($line = fgets($handle)) {
+
+    if(trim($line) == ''){
+        echo "Done, got " . count($newDomains) . " sources, new domains is ";
+
+        $newList = array_merge($semaltBlockerSources, $newDomains);
+        $newList = array_unique($newList);
+        sort($newList);
+
+        echo count($newList) . " sources:\n\n";
+
+        echo implode("\n", $newList) . "\n\n";
+        break;
+    } else {
+        $newDomains[] = trim($line);
+    }
+
+}
---
+++ scripts/merge.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+php ./merge.php
