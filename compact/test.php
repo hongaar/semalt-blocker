11ef099f1af1cb2c5be9a4752d1c811981d21be2
Joram van den Boezem <joram@nabble.nl>
2015-11-30 17:38:56 +0100

    Colored tests in compact script.

--- compact/test.php
+++ compact/test.php
@@ -12,7 +12,7 @@ $_SERVER["HTTP_REFERER"] = 'http://semalt.com';
 try
 {
     // Test blocking
-    $expected = 'Blocking because referral domain (semalt.com) is found on blocklist';
+    $expected = 'Blocking because referer root domain (semalt.com) is found on blocklist';
     $actual = \Nabble\SemaltBlocker\Blocker::blocked(true);
     if (!@assert($expected === $actual)) throw new Exception('Blocking domains failed');
 
@@ -22,12 +22,14 @@ try
     $expected = \Nabble\SemaltBlocker\Updater::getNewDomainList();
     $actual = file_get_contents($BLOCKLIST);
     if (!@assert($expected === $actual)) throw new Exception('Updating domains failed');
+
+    echo chr(27).'[42m'.'All tests passed'.chr(27).'[0m' . PHP_EOL;
 }
 catch (Exception $e)
 {
-    echo $e->getMessage() . '<br/>' . PHP_EOL;
+    echo chr(27).'[41m'.$e->getMessage().chr(27).'[0m' . PHP_EOL;
 }
 
-echo 'All tests finished' . PHP_EOL;
+echo 'Testing finished' . PHP_EOL;
 
 unlink($BLOCKLIST);
