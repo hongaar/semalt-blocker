c686843def316dba3148f36e99779434f2f13015
Joram van den Boezem <joram@nabble.nl>
2016-05-06 10:45:30 -0400

    Applied fixes from StyleCI

--- src/SemaltBlocker/Blocker.php
+++ src/SemaltBlocker/Blocker.php
@@ -1,13 +1,12 @@
 <?php
+
 namespace Nabble\SemaltBlocker;
 
 /**
  * The most important class for this package. Basic usage:
  * ```
  * Blocker::protect();
- * ```
- *
- * @package Nabble\SemaltBlocker
+ * ```.
  */
 class Blocker
 {
@@ -16,33 +15,40 @@ class Blocker
     public static $explanation = "Access to this website has been blocked because your referral is set to %s. <a href='%s'>Read why</a>";
 
     private static $blocklist = './../../domains/blocked';
-    private static $reason    = 'Not blocking, no reason given';
+    private static $reason = 'Not blocking, no reason given';
 
     //////////////////////////////////////////
     // PUBLIC API                           //
     //////////////////////////////////////////
 
     /**
-     * Block a page if referer is found on list of blocked domains
+     * Block a page if referer is found on list of blocked domains.
      *
      * @param string $action If empty, send 403 response; if URL, redirect here; if non-empty string, print message
      */
     public static function protect($action = '')
     {
         // Try to update the list
-        if (!defined('SEMALT_UNIT_TESTING')) Updater::update();
+        if (!defined('SEMALT_UNIT_TESTING')) {
+            Updater::update();
+        }
 
         // Simply stop here if referer is not on the list
-        if (!self::isRefererOnBlocklist()) return;
+        if (!self::isRefererOnBlocklist()) {
+            return;
+        }
 
         self::doBlock($action);
 
         // Stop execution altogether, bye bye bots
-        if (!defined('SEMALT_UNIT_TESTING')) exit;
+        if (!defined('SEMALT_UNIT_TESTING')) {
+            exit;
+        }
     }
 
     /**
      * @param bool $verbose Deprecated. Please use the explain() method instead.
+     *
      * @return bool|string
      */
     public static function blocked($verbose = false)
@@ -51,6 +57,7 @@ class Blocker
         if ($verbose === true) {
             return self::$reason;
         }
+
         return $blocked;
     }
 
@@ -63,12 +70,12 @@ class Blocker
     }
 
     /**
-     * Send a 403 Forbidden header
+     * Send a 403 Forbidden header.
      */
     public static function forbidden()
     {
         $protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
-        header($protocol . ' 403 Forbidden');
+        header($protocol.' 403 Forbidden');
     }
 
     /**
@@ -84,7 +91,7 @@ class Blocker
      */
     public static function getBlocklistFilename()
     {
-        return __DIR__ . DIRECTORY_SEPARATOR . static::$blocklist;
+        return __DIR__.DIRECTORY_SEPARATOR.static::$blocklist;
     }
 
     //////////////////////////////////////////
@@ -92,24 +99,26 @@ class Blocker
     //////////////////////////////////////////
 
     /**
-     * Responsible for sending action output
+     * Responsible for sending action output.
      *
      * @param string $action
      */
     private static function doBlock($action = '')
     {
         // Clear buffered output
-        if (!defined('SEMALT_UNIT_TESTING')) self::cls();
+        if (!defined('SEMALT_UNIT_TESTING')) {
+            self::cls();
+        }
 
         // Take user defined action
         self::blockAction($action);
 
         // If a human comes by, don't just serve a blank page
-        echo sprintf(self::$explanation, self::getHttpReferer(), "https://www.google.com/#q=" . urlencode(preg_replace('/https?:\/\//', '', self::getHttpReferer()) . " referral spam"));
+        echo sprintf(self::$explanation, self::getHttpReferer(), 'https://www.google.com/#q='.urlencode(preg_replace('/https?:\/\//', '', self::getHttpReferer()).' referral spam'));
     }
 
     /**
-     * Execute desired action
+     * Execute desired action.
      *
      * @param string $action
      */
@@ -120,28 +129,31 @@ class Blocker
             self::redirect($action);
         } else {
             self::forbidden();
-            if (!empty($action)) echo $action . '<br/>'; // tell them something nice
+            if (!empty($action)) {
+                echo $action.'<br/>';
+            } // tell them something nice
         }
     }
 
-
     /**
-     * Clear output buffer
+     * Clear output buffer.
      */
     private static function cls()
     {
-        while (ob_get_level()) ob_end_clean();
+        while (ob_get_level()) {
+            ob_end_clean();
+        }
     }
 
     /**
      * Redirect to a url by sending the appropriate header.
+     *
      * @param string $url
      */
     private static function redirect($url)
     {
-        header("Location: " . $url);
+        header('Location: '.$url);
     }
-
 
     /**
      * The public use of this function is undocumented.
@@ -152,20 +164,20 @@ class Blocker
     {
         $referer = self::getHttpReferer();
         if ($referer === null) {
-            self::$reason = "Not blocking because referer header is not set or empty";
+            self::$reason = 'Not blocking because referer header is not set or empty';
 
             return false;
         }
 
-        return self::isUrlOnBlocklist($referer, "referer");
+        return self::isUrlOnBlocklist($referer, 'referer');
     }
-
 
     /**
      * The public use of this function is undocumented.
      *
      * @param string $url
      * @param string $entity
+     *
      * @return bool
      */
     public static function isUrlOnBlocklist($url, $entity = 'url')
@@ -178,39 +190,40 @@ class Blocker
         }
 
         $blocklist = self::getConcatenateBlocklist();
-        if (substr_count($blocklist, self::SEPERATOR . $rootDomain . self::SEPERATOR)) {
-            self::$reason = "Blocking because " . $entity . " root domain (" . $rootDomain . ") is found on blocklist";
+        if (substr_count($blocklist, self::SEPERATOR.$rootDomain.self::SEPERATOR)) {
+            self::$reason = 'Blocking because '.$entity.' root domain ('.$rootDomain.') is found on blocklist';
 
             return true;
         }
 
         $hostname = Domainparser::getHostname($url);
-        if (substr_count($blocklist, self::SEPERATOR . $hostname . self::SEPERATOR)) {
-            self::$reason = "Blocking because " . $entity . " hostname (" . $hostname . ") is found on blocklist";
+        if (substr_count($blocklist, self::SEPERATOR.$hostname.self::SEPERATOR)) {
+            self::$reason = 'Blocking because '.$entity.' hostname ('.$hostname.') is found on blocklist';
 
             return true;
         }
 
         $path = Domainparser::getPath($url);
         if (trim($path, '/')) {
-            if (substr_count($blocklist, self::SEPERATOR . $rootDomain . $path . self::SEPERATOR)) {
-                self::$reason = "Blocking because " . $entity . " root domain/path (" . $rootDomain . $path . ") is found on blocklist";
+            if (substr_count($blocklist, self::SEPERATOR.$rootDomain.$path.self::SEPERATOR)) {
+                self::$reason = 'Blocking because '.$entity.' root domain/path ('.$rootDomain.$path.') is found on blocklist';
 
                 return true;
             }
-            if (substr_count($blocklist, self::SEPERATOR . $hostname . $path . self::SEPERATOR)) {
-                self::$reason = "Blocking because " . $entity . " hostname/path (" . $hostname . $path . ") is found on blocklist";
+            if (substr_count($blocklist, self::SEPERATOR.$hostname.$path.self::SEPERATOR)) {
+                self::$reason = 'Blocking because '.$entity.' hostname/path ('.$hostname.$path.') is found on blocklist';
 
                 return true;
             }
         }
 
-        self::$reason = "Not blocking because " . $entity . " (" . $url . ") is not matched against blocklist";
+        self::$reason = 'Not blocking because '.$entity.' ('.$url.') is not matched against blocklist';
+
         return false;
     }
 
     /**
-     * Returns HTTP Referer if it is available and not empty, null otherwise
+     * Returns HTTP Referer if it is available and not empty, null otherwise.
      *
      * @return string|null
      */
@@ -219,7 +232,6 @@ class Blocker
         if (isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER'])) {
             return $_SERVER['HTTP_REFERER'];
         }
-        return null;
     }
 
     /**
@@ -228,6 +240,7 @@ class Blocker
     private static function getBlocklistContents()
     {
         $blocklistContent = file_get_contents(self::getBlocklistFilename());
+
         return $blocklistContent;
     }
 
@@ -241,6 +254,7 @@ class Blocker
 
     /**
      * @param string $blocklistContent
+     *
      * @return array
      */
     private static function parseBlocklist($blocklistContent)
@@ -250,10 +264,11 @@ class Blocker
 
     /**
      * @param string $blocklistContent
+     *
      * @return string
      */
     private static function concatenateBlocklist($blocklistContent)
     {
-        return self::SEPERATOR . str_replace(PHP_EOL, self::SEPERATOR, strtolower($blocklistContent)) . self::SEPERATOR;
+        return self::SEPERATOR.str_replace(PHP_EOL, self::SEPERATOR, strtolower($blocklistContent)).self::SEPERATOR;
     }
 }
